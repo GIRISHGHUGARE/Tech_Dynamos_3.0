05d2f177c88e0a6b52ccd4794b5d04addfb3a5b3
Tushar <tushar.hasule@somaiya.edu>
2024-08-10 11:53:56 +0530

    Tailwind add and removed some useless files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
-import Home from './screens/Home';
+import React from 'react';
+import Home from './screens/Home'
+
 
 function App() {
   return (
     <>
       <div>
-        <Home />
+        <Home/>
       </div>
     </>
   );
