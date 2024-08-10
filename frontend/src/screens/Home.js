9971a5ca7e741f8a9dacb6a54c979d522e7ec48b
Girish Ghugare <girishghugare19@gmail.com>
2024-08-10 15:40:30 +0530

    Body changes

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -11,13 +11,7 @@ export default function Home() {
     return (
         <>
             <div>
-                <Navbar />
-            </div>
-            <div>
              Body
-            </div>
-            <div>
-                <Footer />
             </div>
         </>
     )
