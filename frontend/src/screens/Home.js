f7807f93a01abdd7aa1665dbf62fab239732ef2d
Tushar <tushar.hasule@somaiya.edu>
2024-08-10 14:42:49 +0530

    Basic auth done and frontend connected to backend

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import SignupForm from './SignUp'
+import LoginForm from './Login'
 
 export default function Home() {
+    
+    const [auth, setAuth] = useState(false);
+
     return (
         <>
             <div>
                 <Navbar />
             </div>
             <div>
-                <div className="container">
+                <div>
+                    <SignupForm />
+                    <LoginForm setAuth={setAuth} />
+                </div>
+                {/* <div className="container">
                     <div className="title">
                         <h3>Real Alerts and Response</h3>
                         <p>Know Weather Forecasts and Disaster Alerts</p>
@@ -30,10 +39,10 @@ export default function Home() {
                             </button>
                         </div>
                     </div>
-                </div>
+                </div> */}
             </div>
             <div>
-                <Footer/>
+                <Footer />
             </div>
         </>
     )
