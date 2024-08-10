b8cd06421034b855fd4048ce7770a8a8320686c3
Tushar <tushar.hasule@somaiya.edu>
2024-08-10 13:59:22 +0530

    Backend CRUD operation successfull for user auth

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,79 @@
+const User = require('../models/userSchema.js');
+
+const addUser = async (req, res) => {
+    try {
+        const { name, email, mobile, password, city } = req.body;
+
+        const newUser = new User({
+            name,
+            email,
+            mobile,
+            password,
+            city
+        });
+
+        await newUser.save();
+
+        res.status(201).json({ message: 'User created successfully', user: newUser });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+}
+
+const findUsers = async (req, res) => {
+    const findUser = await User.find()
+    res.json({
+        success: true,
+        data: findUser,
+    });
+}
+
+const deleteUser = async (req, res) => {
+    const duser = await User.findByIdAndDelete(req.params.id);
+    res.json({
+        success: true,
+        data: duser
+    })
+}
+
+const updateUser = async (req, res) => {
+    const uUser = await User.findByIdAndUpdate(req.params.id, req.body);
+    res.json({
+        success: true,
+        data: uUser
+    })
+}
+
+
+const login = async (req, res) => {
+    try {
+
+        const { email, password } = req.body;
+
+        const userEmail = await User.findOne({ email });
+        
+        if (!userEmail) {
+            return res.json({
+                "User-email": "Not Found!!"
+            });
+        }
+
+        if (password !== userEmail.password) {
+            return res.json({
+                "User-password": "Incorrect Password!!"
+            });
+        }
+
+        return res.json({
+            message: "validUser!!"
+        });
+
+    } catch (error) {
+        console.error("Error in login:", error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+
+
+module.exports = { addUser, findUsers, deleteUser, updateUser, login };
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,22 @@ const express = require('express')
 const dotenv = require('dotenv')
 dotenv.config()
 
+const cors = require('cors')
+const bodyPaser = require('body-parser')
+
 const connectDb = require('./connection')
+const userRouter = require('./routes/userRouter.js')
 
 const app = express()
 
+
 connectDb()
 
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT, () => {
     console.log(`Server running at http://localhost:${process.env.PORT}`);
 })
 
+app.use(cors())
+app.use(bodyPaser.json())
+
+app.use('/api/auth',userRouter);
