1569c3fbd3845d7a7a97eb8e85e8814f9b03758e
Girish Ghugare <girishghugare19@gmail.com>
2024-08-11 00:39:39 +0530

    login screen design added

--- frontend/src/screens/Login.jsx
+++ frontend/src/screens/Login.jsx
@@ -1,6 +1,7 @@
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import "../Login.css"
 
 const LoginForm = () => {
     const { register, handleSubmit } = useForm();
@@ -19,11 +20,40 @@ const LoginForm = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register('email')} type="email" placeholder="Email" />
-            <input {...register('password')} type="password" placeholder="Password" />
-            <button type="submit">Login</button>
-        </form>
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input {...register('email')} type="email" placeholder="Email" />
+                <input {...register('password')} type="password" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+            <div className='login'>
+                <div className="loginForm">
+                    <div className="header">
+                        <span className='title'>Login / Signup</span>
+                        <span className="desc">Now you can login get access to
+                            <br /><span>community and chats</span>
+                        </span>
+                    </div>
+                    <div className="form">
+                        <span className='label'>Login</span>
+                        <div className="email">
+                            <label>Enter your Email : </label>
+                            <input type='text'></input>
+                        </div>
+                        <div className="password">
+                            <label>Enter your Password : </label>
+                            <input type='text'></input>
+                        </div>
+                        <div className="button">
+                            <a href="">Submit</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="formImg">
+                    <img src="./login.jpg" />
+                </div>
+            </div>
+        </>
     );
 };
 
